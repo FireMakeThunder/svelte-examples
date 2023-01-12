be202cc82131af7942bb5529001296a176b256a6
FireMakeThunder <31500866+FireMakeThunder@users.noreply.github.com>
2023-01-12 03:53:00 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# svelte-examples
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-hthrrb)
---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import "./app.css";
+
+  // Track the operands and operations, in the order they're received.
+
+</script>
+
+<main class="grid grid-cols-4">
+
+<section id="numbers" class="grid grid-cols-3">
+{#each Array(9) as _, number}
+  <button>{number}</button>
+{/each}
+  <button class="col-span-2">0</button>
+  <button class="">.</button>
+</section>
+
+</main>
+
+<button>+</button>
+
+<style>
+  button {
+    @apply grid place-items-center;
+    border-radius: 9999px;
+  }
+</style>
