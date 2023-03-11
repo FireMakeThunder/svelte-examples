f0b55165529027a1b0e16388d97d76751fcc55a6
Elias Fraley <elias.fraley@gmail.com>
2023-03-11 00:17:33 -0800

    Adds means to prevent going to the htmx routes via browser

--- htmx/src/hooks.server.ts
+++ htmx/src/hooks.server.ts
@@ -1,12 +1,16 @@
-import type { Handle } from "@sveltejs/kit";
+import { error, type Handle } from "@sveltejs/kit";
+import { sequence } from "@sveltejs/kit/hooks";
 
-export const handle = (({event, resolve}) => {
-  console.log('serverhandle', event.url.pathname);
-
-  // if (event.request.headers.has('hx-request')) {
-    // return new Response('Response from the handle')
-  // } else {
+/** contrived means to prevent going to the htmx routes via browser */
+const stopDirectAccessToHTMXRoute = (({event, resolve}) => {
+  const {request} = event;
+  const url = new URL(request.url);
+  if (url.pathname.startsWith('/htmx') && !request.headers.has('hx-request')) {
+    throw error(403);
+  } else {
     return resolve(event);
-  // }
-
+  }
 }) satisfies Handle;
+
+
+export const handle = sequence(stopDirectAccessToHTMXRoute);
