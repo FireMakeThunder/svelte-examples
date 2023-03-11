bf4306d00adb625394b92f9835b9315f069928f8
Elias Fraley <elias.fraley@gmail.com>
2023-03-11 00:09:26 -0800

    First real test of requesting and swapping out with a svelte +page

--- htmx/src/hooks.server.ts
+++ htmx/src/hooks.server.ts
@@ -3,10 +3,10 @@ import type { Handle } from "@sveltejs/kit";
 export const handle = (({event, resolve}) => {
   console.log('serverhandle', event.url.pathname);
 
-  if (event.request.headers.has('hx-request')) {
-    return new Response('Response from the handle')
-  } else {
+  // if (event.request.headers.has('hx-request')) {
+    // return new Response('Response from the handle')
+  // } else {
     return resolve(event);
-  }
+  // }
 
 }) satisfies Handle;
---
+++ htmx/src/routes/htmx/test/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+  import { spring }  from 'svelte/motion';
+
+  const a = spring(0);
+  setTimeout(() => $a = 100, 0);
+</script>
+
+
+<div class="relative" style:top="{$a}px">
+  Wait
+</div>
